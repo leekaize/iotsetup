718afccaa3f454e270782b02c7a5b558f9b631c9
Lee Kai Ze <kzlee.my@gmail.com>
2023-09-06 07:57:14 +0800

    Enhanced input read

--- base/base_setup.sh
+++ base/base_setup.sh
@@ -10,23 +10,28 @@ sudo mkdir -p /usr/local/sbin/base/portainer/data
 sudo chown -R 9000:9000 /usr/local/sbin/base/portainer
 
 sudo cp $gitdir/docker-compose.yml ./
-echo "Root domain: "
-read domain
+read -p "Enter domain name (e.g. mywebsite.com): " domain
 export domain
 envsubst < ./docker-compose.yml | sudo tee "./docker-compose.yml"
 
 sudo cp $gitdir/traefik_api.yml ./traefik/traefik_api.yml
-echo "Traefik username: "
-read username
+read -p "Set Traefik username: " username
 export username
-echo "Traefik password: "
-read password
+
+while true; do
+  read -p "Set Traefik password: " -s password
+  echo
+  read -p "Confirm password: " -s password2
+  echo
+  [ "$password" = "$password2" ] && break
+  echo "Password not match, please try again."
+done
 export password
+
 export secret=$(docker run --rm httpd htpasswd -nb $username $password)
 envsubst < ./traefik/traefik_api.yml | sudo tee "./traefik/traefik_api.yml"
 
-echo "Email for TLS: "
-read email
+read -p "Set Email for TLS cert: " email
 export email
 sudo cp $gitdir/traefik.yml ./traefik/traefik.yml
 envsubst < ./traefik/traefik.yml | sudo tee "./traefik/traefik.yml"
