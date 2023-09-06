2a7b5211f6361c0e7d15532d2c8046026807bd24
Lee Kai Ze <kzlee.my@gmail.com>
2023-09-06 08:04:30 +0800

    Cleaner read test

--- base/base_setup.sh
+++ base/base_setup.sh
@@ -11,13 +11,12 @@ sudo chown -R 9000:9000 /usr/local/sbin/base/portainer
 
 sudo cp $gitdir/docker-compose.yml ./
 read -p "Enter domain name (e.g. mywebsite.com): " domain
-export domain
+# export domain
 envsubst < ./docker-compose.yml | sudo tee "./docker-compose.yml"
 
 sudo cp $gitdir/traefik_api.yml ./traefik/traefik_api.yml
 read -p "Set Traefik username: " username
-export username
-
+# export username
 while true; do
   read -p "Set Traefik password: " -s password
   echo
@@ -26,15 +25,15 @@ while true; do
   [ "$password" = "$password2" ] && break
   echo "Password not match, please try again."
 done
-export password
-
+# export password
 export secret=$(docker run --rm httpd htpasswd -nb $username $password)
 envsubst < ./traefik/traefik_api.yml | sudo tee "./traefik/traefik_api.yml"
 
 read -p "Set Email for TLS cert: " email
-export email
+# export email
 sudo cp $gitdir/traefik.yml ./traefik/traefik.yml
 envsubst < ./traefik/traefik.yml | sudo tee "./traefik/traefik.yml"
 
+docker image rm httpd
 docker compose up -d
 
