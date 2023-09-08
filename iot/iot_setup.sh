afdfc80a82c81c54c8af613d2b6dfe04c6a765cc
Lee Kai Ze <kzlee.my@gmail.com>
2023-09-08 08:14:30 +0800

    Mosquitto password setup

--- iot/iot_setup.sh
+++ iot/iot_setup.sh
@@ -11,4 +11,22 @@ export domain
 envsubst < $gitdir/docker-compose.yml | sudo tee "$iotdir/docker-compose.yml"
 
 sudo cp $gitdir/mosquitto/config/mosquitto.conf $iotdir/mosquitto/config/mosquitto.conf
+echo "#" | sudo tee $iotdir/mosquitto/config/passwordfile
+sudo chown -R 8883:8883 $iotdir/mosquitto
+
+docker-compose up -d
+read -p "Set Mosquitto username: " username
+while true; do
+  read -p "Set Mosquitto password: " -s password
+  echo
+  read -p "Confirm password: " -s password2
+  echo
+  [ "$password" = "$password2" ] && break
+  echo "Password not match, please try again."
+done
+
+docker exec -it mosquitto mosquitto_passwd -c /mosquitto/config/passwordfile -b $username $password
+docker exec -it mosquitto chown root:root /mosquitto/config/passwordfile
+docker exec -it mosquitto chmod 600 /mosquitto/config/passwordfile
+docker-compose restart mosquitto
 
