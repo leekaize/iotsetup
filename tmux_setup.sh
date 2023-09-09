3be2006161e083c3dfc9f3027125fbe6a83de185
Lee Kai Ze <kzlee.my@gmail.com>
2023-09-09 15:43:08 +0800

    Autostart tmux

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -3,3 +3,12 @@
 sudo apt install zsh tmux -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "set -g mouse on" >> ~/.tmux.conf
+
+cat >> ~/.zshrc << EOL
+if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
+  tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
+fi
+
+EOL
+tmux new-session -s ssh_tmux
+
