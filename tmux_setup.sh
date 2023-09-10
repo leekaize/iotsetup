1789c7d8d5b87dff23145c60a113439a1b707117
Lee Kai Ze <kzlee.my@gmail.com>
2023-09-10 13:22:07 +0800

    Give instruction

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt install zsh tmux -y
 echo "set -g mouse on" >> ~/.tmux.conf
+read -p "After installation, re-login to update config. Press Enter to continue..." </dev/tty
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cat >> ~/.zshrc << EOL
@@ -10,6 +11,4 @@ if [[ \$- =~ i ]] && [[ -z "\$TMUX" ]] && [[ -n "\$SSH_TTY" ]]; then
 fi
 
 EOL
-
-exit
 
